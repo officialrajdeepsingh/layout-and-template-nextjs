f61f86a091066ccaa36c85031714ca9a61af6780
Rajdeep Singh <radewala.2@gmail.com>
2024-01-09 17:12:49 +0530

    about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <section className="bg-white dark:bg-gray-900">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+              
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                    About Page
+                    </h1>
+                <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here is example we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+            </div>
+        </section>
+    )
+}
