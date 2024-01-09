16ecf7e44e97f06c4c07ae7a45473dc5b61cf2f7
Rajdeep Singh <radewala.2@gmail.com>
2024-01-09 16:45:01 +0530

    Update Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
-import { Card } from "@/components/Card"
-
-async function getPosts(searchParams: { q: string | string[] | undefined }) {
-
-  let getURL = searchParams.q !== undefined ? `http://localhost:3000/api/posts?q=${searchParams.q}` : `http://localhost:3000/api/posts`
-
-  const res = await fetch(getURL, { cache: 'no-store' })
-
-  return res.json()
-}
-
-export default async function Home({ searchParams }: { searchParams: { q: string | string[] | undefined } }) {
-
-  let { posts } = await getPosts(searchParams)
-
-  return posts.map((post: { id: number; title: string; body: string; userId: number; tags: string[]; reactions: number; }) => {
-    return <Card key={post.id} post={post} />
-  })
-
+export default function Home() {
+  return (
+      <section className="bg-white dark:bg-gray-900">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+            
+              <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                  Home Page
+                  </h1>
+              <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here is example we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          </div>
+      </section>
+  )
 }
